4757de3e7a1c143580b93e9328583599b1b33f32
Hamza Sultan <hamzasultan1702@gmail.com>
2026-07-22 09:09:38 +0500

    updates deploy.yml to overcome the MU-plugin intercept

--- site/wp-content/mu-plugins/samina-core/opcache-reset.php
+++ site/wp-content/mu-plugins/samina-core/opcache-reset.php
@@ -1,11 +1,13 @@
 <?php
-// Secret endpoint to reset OPcache after atomic deploy
-if (defined('SR_OPCACHE_SECRET') && !empty(SR_OPCACHE_SECRET) && isset($_GET['token']) && hash_equals(SR_OPCACHE_SECRET, $_GET['token'])) {
-    if (function_exists('opcache_reset')) {
-        opcache_reset();
+// Only execute if this is specifically an OPcache reset request
+if (isset($_GET['sr_opcache_reset']) && $_GET['sr_opcache_reset'] === '1') {
+    if (defined('SR_OPCACHE_SECRET') && !empty(SR_OPCACHE_SECRET) && isset($_GET['token']) && hash_equals(SR_OPCACHE_SECRET, $_GET['token'])) {
+        if (function_exists('opcache_reset')) {
+            opcache_reset();
+        }
+        http_response_code(200);
+        die('OPcache reset successful');
     }
-    http_response_code(200);
-    die('OPcache reset successful');
+    http_response_code(403);
+    die('Forbidden');
 }
-http_response_code(403);
-die('Forbidden');
