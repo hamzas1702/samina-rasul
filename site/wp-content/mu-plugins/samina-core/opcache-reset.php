acb8d98d2de75cc5dd9ce020cb4e4b5875d028a4
Hamza Sultan <hamzasultan1702@gmail.com>
2026-07-22 08:47:37 +0500

    OPcache-reset fix

--- site/wp-content/mu-plugins/samina-core/opcache-reset.php
+++ site/wp-content/mu-plugins/samina-core/opcache-reset.php
@@ -1,6 +1,6 @@
 <?php
 // Secret endpoint to reset OPcache after atomic deploy
-if (defined('SR_OPCACHE_SECRET') && !empty(SR_OPCACHE_SECRET) && isset($_SERVER['HTTP_X_SR_RESET_TOKEN']) && hash_equals(SR_OPCACHE_SECRET, $_SERVER['HTTP_X_SR_RESET_TOKEN'])) {
+if (defined('SR_OPCACHE_SECRET') && !empty(SR_OPCACHE_SECRET) && isset($_GET['token']) && hash_equals(SR_OPCACHE_SECRET, $_GET['token'])) {
     if (function_exists('opcache_reset')) {
         opcache_reset();
     }
